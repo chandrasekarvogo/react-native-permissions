4bf5c67c41308ed21742e4f55edddb40615d20a2
Mathieu Acthernoene <zoontek@gmail.com>
2019-05-04 21:32:53 +0200

    Drop support for iOS 9.3

--- ios/PermissionHandlers/StoreKit/RNPermissionHandlerStoreKit.m
+++ ios/PermissionHandlers/StoreKit/RNPermissionHandlerStoreKit.m
@@ -6,32 +6,24 @@
 
 - (void)checkWithResolver:(void (^)(RNPermissionStatus status))resolve
              withRejecter:(void (__unused ^)(NSError *error))reject {
-  if (@available(iOS 9.3, *)) {
-    switch ([SKCloudServiceController authorizationStatus]) {
-      case SKCloudServiceAuthorizationStatusNotDetermined:
-        return resolve(RNPermissionStatusNotDetermined);
-      case SKCloudServiceAuthorizationStatusRestricted:
-        return resolve(RNPermissionStatusRestricted);
-      case SKCloudServiceAuthorizationStatusDenied:
-        return resolve(RNPermissionStatusDenied);
-      case SKCloudServiceAuthorizationStatusAuthorized:
-        return resolve(RNPermissionStatusAuthorized);
-    }
-  } else {
-    resolve(RNPermissionStatusNotAvailable);
+  switch ([SKCloudServiceController authorizationStatus]) {
+    case SKCloudServiceAuthorizationStatusNotDetermined:
+      return resolve(RNPermissionStatusNotDetermined);
+    case SKCloudServiceAuthorizationStatusRestricted:
+      return resolve(RNPermissionStatusRestricted);
+    case SKCloudServiceAuthorizationStatusDenied:
+      return resolve(RNPermissionStatusDenied);
+    case SKCloudServiceAuthorizationStatusAuthorized:
+      return resolve(RNPermissionStatusAuthorized);
   }
 }
 
 - (void)requestWithOptions:(__unused NSDictionary * _Nullable)options
               withResolver:(void (^)(RNPermissionStatus status))resolve
               withRejecter:(void (^)(NSError *error))reject {
-  if (@available(iOS 9.3, *)) {
-    [SKCloudServiceController requestAuthorization:^(__unused SKCloudServiceAuthorizationStatus status) {
-      [self checkWithResolver:resolve withRejecter:reject];
-    }];
-  } else {
-    resolve(RNPermissionStatusNotAvailable);
-  }
+  [SKCloudServiceController requestAuthorization:^(__unused SKCloudServiceAuthorizationStatus status) {
+    [self checkWithResolver:resolve withRejecter:reject];
+  }];
 }
 
 @end
